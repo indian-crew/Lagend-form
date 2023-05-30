17a31990868c3dc756d487cc0bfa991f9d245ce5
indian-crew <105585465+indian-crew@users.noreply.github.com>
2023-05-30 18:41:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function fa(){ 
+if(a.value=="" || b.value==""){ 
+f() 
+document.getElementById("a").style.border="3px solid red" 
+document.getElementById("b").style.border="3px solid red" 
+bt.value="pahle data dalo" 
+} 
+else{ 
+document.getElementById("a").style.border="3px solid green" 
+document.getElementById("b").style.border="3px solid green" 
+bt.value="Ha ab thik hai" 
+bt.style.left="120px"; 
+} 
+} 
+flag=1 
+function f(){ 
+if(flag==1){ 
+bt.style.left="210px" 
+flag=2 
+} 
+else if(flag==2){ 
+bt.style.left="80px" 
+flag=1 
+} 
+} 
